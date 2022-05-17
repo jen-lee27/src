02d976ab091870391883a337c34ff647553db42f
jen-lee27 <jlee689@myseneca.ca>
2022-05-17 03:36:44 -0400

    updated with new style.css

--- jenlee/js/script.js
+++ jenlee/js/script.js
@@ -1,4 +1,14 @@
 // upon page load
 addEventListener("DOMContentLoaded", function () {
-    
+    let header = document.getElementById("nav");
+    let headerOffset = header.offsetTop;
+    window.addEventListener("scroll", function() {
+        let scrollTop = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+
+        console.log(scrollTop);
+        if(scrollTop >= headerOffset)
+            header.classList.add("on");
+        else
+            header.classList.remove("on");
+    });
 });
